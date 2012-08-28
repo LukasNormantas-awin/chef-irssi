6648fc7f5679d40cdb96cbca7aa8ccd858c2c17d
Trond Arve Nordheim <tanordheim@gmail.com>
2012-08-28 14:47:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## DESCRIPTION
+
+Installs and configures irssi on a node.
+
+## REQUIREMENTS
+
+Only tested on Debian 6.0.
+
+## USAGE
+
+The irssi:default recipe installs irssi on the node.
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+maintainer       'Trond Arve Nordheim'
+maintainer_email 't@binarymarbles.com'
+license          'Apache 2.0'
+description      'Installs and configures Irssi'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
+
+supports         'debian'
+
+recipe           'irssi', 'Installs and configures Irssi'
